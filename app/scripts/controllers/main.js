aa0d9f3c5e5772eacef9e328390fc2755a95a190
DANI-VAIO\Danita <dennieru.com@gmail.com>
2016-08-22 23:50:44 -0400

    build grunt

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name danitaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the danitaApp
+ */
+angular.module('danitaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
