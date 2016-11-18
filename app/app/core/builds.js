f637908c4bdfd757d7c40cb68d185c046bb02585
Daniel Pacheco Villarpando <daniel.pacheco@jalasoft.com>
2016-11-18 10:39:41 -0400

    :octocat: Eslint config.

---
+++ app/app/core/builds.js
@@ -0,0 +1,12 @@
+/* global toastr:false, moment:false */
+
+(function () {
+    'use strict';
+    
+    var core = angular.module('xianmp.core');
+
+    core.constant('builds', {
+        console: '1.0.0.0',
+        services: '6.0.0.0'
+    });
+})();
---
+++ config/builds.js
@@ -0,0 +1,12 @@
+/* global toastr:false, moment:false */
+
+(function () {
+    'use strict';
+    
+    var core = angular.module('xianmp.core');
+
+    core.constant('builds', {
+        console: '@@console',
+        services: '@@services'
+    });
+})();
