b4ade185c33bf37580a03887de0a7e24fc16fb9a
DANI-VAIO\Danita <dennieru.com@gmail.com>
2016-11-19 20:52:45 -0400

    :zap: Fixing container & component

--- app/src/layout/main/main.js
+++ app/src/layout/main/main.js
@@ -11,11 +11,24 @@ angular.module('danitaApp')
   .controller('MainCtrl', function () {
     var vm = this;
 
+    vm.fullContainer = false;
+    
+    // Methods
+    vm.activateFullContainer = activateFullContainer;
+
     $(document).ready(function(){
       $('.collapsible').collapsible({
         accordion : false
       });
     });
+        
+    function activateFullContainer(){
+        vm.fullContainer = !vm.fullContainer;
+        $( "#dnt-container" ).toggleClass( "container" );
+        //var element = angular.element( document.querySelector( '#dnt-container' ) );
+        //element.addClass('container');
+        //document.getElementById("peace").className = "be-still"
+    };
 
     vm.awesomeThings = [
       'HTML5 Boilerplate',
