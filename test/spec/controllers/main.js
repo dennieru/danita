bcdde6c53030efcb5e95a5b484384fcaabd990b2
JALASOFT\Daniel Pacheco <Daniel.Pacheco@jalasoft.com>
2017-10-27 16:05:11 -0400

    :octocat: Uploading version angular 4 project with materialize

--- test/spec/controllers/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-describe('Controller: LayoutCtrl', function () {
-  var imTrue = true;
-
-  it('should be a very very easy test', function () {
-    expect(imTrue).toBe(true);
-  });
-});
